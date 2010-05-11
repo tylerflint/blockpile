e69eda5319704c2be7fa83234b83b379692469a3
Tyler Flint <tylerflint@Delorum10.local>
2010-05-10 21:50:19 -0600

    adding initializer code

--- lib/blockpile.rb
+++ lib/blockpile.rb
@@ -1,1 +1,23 @@
-# Blockpile
+class Blockpile
+  
+  def initialize(helper)
+    @helper = helper
+    @path   = '/app/views/piles/'
+  end
+  
+  def to_html
+    render_template @template
+  end
+  
+protected
+  
+  # Assumes /views/helper/ as base
+  def render_template(template)
+    ERB.new( File.read(RAILS_ROOT + @path + template + ".html.erb") ).result binding
+  end
+  
+  def method_missing(*args, &block)
+    @helper.send(*args, &block)
+  end
+  
+end
