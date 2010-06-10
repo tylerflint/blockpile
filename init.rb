b2353d50825760f2ef88cd2003d2122d2fe6bd5c
Tyler Flint <tylerflint@Delorum10.local>
2010-06-10 00:22:38 -0600

    fixing a conflict in init.rb

--- init.rb
+++ init.rb
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-# Include hook code here
-require 'blockpile'
-
-Dir.glob(Rails.root.to_s + '/app/blockpiles/*') {|file| require file}
-=======
 # include our custom helper in ActionView
 path = File.join(File.dirname(__FILE__), 'lib', 'app', 'helpers') 
 $LOAD_PATH << path 
 ActiveSupport::Dependencies.load_paths << path 
 ActiveSupport::Dependencies.load_once_paths.delete(path) 
 ActionView::Base.send :include, BlockpilesHelper
->>>>>>> f2130f7
