f96347604ad6f869d498a535f3a230d117c436bf
Tyler Flint <tylerflint@Delorum10.local>
2010-08-13 16:55:06 -0600

    ensuring we don't yield a block if none given

--- lib/blockpile/setup.rb
+++ lib/blockpile/setup.rb
@@ -7,9 +7,9 @@ module Blockpile
         class_name  = file_name.classify
         ActionView::Base.class_eval %{
           def #{file_name}(*args, &block)
-              block = #{class_name}.new(self, session, params, '#{file_name}', *args)
-              yield block
-              raw block.to_html
+              blockpile = #{class_name}.new(self, session, params, '#{file_name}', *args)
+              yield blockpile if block_given?
+              raw blockpile.to_html
           end
         }
       end
