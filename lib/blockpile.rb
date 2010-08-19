ab9ab09f8fe90efa9f5e3031d3549451f4c0c56d
Ryan Dew <ryan@Delorum13s-iMac.local>
2010-08-19 16:18:47 -0600

    Altered Blockpile to use render partial

--- lib/blockpile.rb
+++ lib/blockpile.rb
@@ -1,15 +1,13 @@
 require 'rails'
 require 'blockpile/setup'
 require 'blockpile/base'
-require 'blockpile/paths'
 
 module Blockpile
   class Railtie < Rails::Railtie
     initializer "blockpile.setup default directories" do
       Blockpile.setup do |config|
-        config.add_load_path Rails.root.to_s + '/app/helpers/blockpiles'
+        config.add_load_path Rails.root.to_s + '/app/helpers'
       end
-      Blockpile::Paths.add_template_path Rails.root.to_s + '/app/views/blockpiles/'
     end
   end
   
--- lib/blockpile/setup.rb
+++ lib/blockpile/setup.rb
@@ -1,12 +1,12 @@
 module Blockpile
   module Setup
     def self.add_load_path(path)
-      Dir.glob(path + "/**/*.rb") do |file|
+      Dir.glob(path + "/**/*_pile.rb") do |file|
         ActiveSupport::Dependencies.autoload_paths << File.dirname(file)
-        file_name   = file.split(/\//).pop.gsub(/.rb/, '')
+        file_name   = file.split(/\//).pop.gsub(/\.rb/, '')
         class_name  = file_name.classify
         ActionView::Base.class_eval %{
-          def #{file_name}(*args, &block)
+          def #{file_name.gsub(/\_pile/, '')}(*args, &block)
               blockpile = #{class_name}.new(self, session, params, '#{file_name}', *args, &block)
               raw blockpile.to_html
           end
